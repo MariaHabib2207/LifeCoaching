0d2bd75e2489ce20b5b254348c491c073f52c7bc
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2023-08-26 03:09:33 +0500

    Design issue fixes

---
+++ static/js/script.js
@@ -0,0 +1,37 @@
+
+// ---------------------------Our news section popup-------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Event listner')
+    const seeMoreButtons = document.querySelectorAll('.see-more-button');
+    const popup = document.getElementById('popup');
+    const popupImage = document.getElementById('popupImage');
+    const popupTitle = document.getElementById('popupTitle');
+    const popupDescription = document.getElementById('popupDescription');
+    const closePopup = document.getElementById('closePopup');
+    
+    seeMoreButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const image = button.getAttribute('data-image');
+            const title = button.getAttribute('data-title');
+            const description = button.getAttribute('data-description');
+            popupImage.src = baseUrl +image;
+            popupTitle.textContent = title;
+            popupDescription.textContent = description;
+            
+            popup.style.display = 'block';
+        });
+    });
+    
+    closePopup.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+    
+    window.addEventListener('click', (event) => {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
+
